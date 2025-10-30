a558ce04bf5d2476050d0ce38d2c940a48f33388
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-10-30 22:18:44 +0300

    Cards

--- cib/src/common/Card.jsx
+++ cib/src/common/Card.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import './Card.css';
+import Icon from '../assets/icon.png'; 
 
 const Card = () => {
     return ( 
 
-        <p>h</p>
+        <>
+        <div className='flex'>
+            <div className='card1'>
+                <img src={Icon} alt="CIB Logo" className="cib-logo" />        
+                <h2>Apply for a new account</h2>
+            </div>
+
+              <div className='card1'>
+                <img src={Icon} alt="CIB Logo" className="cib-logo" />        
+                <h2>Apply for a card</h2>
+            </div>
+
+              <div className='card1'>
+                <img src={Icon} alt="CIB Logo" className="cib-logo" />        
+                <h2>Apply for loan or overdraft</h2>
+            </div>
+        </div>
+        
+        
+        
+        
+        </>
      );
 }
  
