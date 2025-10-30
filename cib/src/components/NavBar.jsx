f00999592a0a1eb351cd91c2725b6af1d3cdcf2b
mariamwaleed05 <mariam.waleed2005@gmail.com>
2025-10-30 09:41:28 +0300

    Header

--- cib/src/components/NavBar.jsx
+++ cib/src/components/NavBar.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './NavBar.css';
+import CibLogo from '../assets/ciblogo.png'; 
 
 const NavBar = () => {
     return ( 
 
-        <p>h</p>
+            <header className="header-container">
+      <div className="header-content">
+        <img src={CibLogo} alt="CIB Logo" className="cib-logo" />        <nav className="main-nav">
+          <ul className="nav-list">
+            <li className="nav-item active">About Us</li>
+            <li className="nav-item">Investor Relations</li>
+            <li className="nav-item">Responsible Banking</li>
+            <li className="nav-item">Newsroom</li>
+            <li className="nav-item">Learning Center</li>
+            <li className="nav-item">Careers</li>
+            <li className="nav-item">Others</li>
+          </ul>
+        </nav>
+      </div>
+    </header> 
+
+
+
+
+
      );
 }
  
